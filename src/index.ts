4c34fc69f69786cb563a4ba4b02d726ce5eae891
takuminish <takuminv1997@gmail.com>
2024-05-04 23:21:24 +0900

    routingの例と@hono/zod-validatorの使用

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,78 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+// GET 第一引数にpath
+app.get("/", (c) => {
+  return c.json({ result: "get" }, 200);
+});
 
-export default app
+app.get("/:id", (c) => {
+  return c.json({ result: c.req.param("id") });
+});
+
+app.post("/", (c) => {
+  return c.json({ result: "post" }, 201);
+});
+
+app.get("/not-found", (c) => {
+  return c.json({ result: "not-found" }, 404);
+});
+
+app.post("/conflict", (c) => {
+  return c.json({ result: "conflict" }, 409);
+});
+
+const nestApp = new Hono();
+nestApp.get("/", (c) => {
+  return c.json({ result: "nest" });
+});
+
+// zodを使用したバリデーションには@hono/zod-validatorを使用する
+const zodValidatorApp = new Hono();
+const schema = z.object({
+  id: z.string().min(6, { message: "6桁以上にしてください" }),
+});
+// validation successの場合
+// {
+//   "data": {
+//       "id": "123456"
+//   },
+//   "success": true
+// }
+// validation errorの場合
+// {
+//   "data": {
+//       "id": "e"
+//   },
+//   "success": false,
+//   "error": {
+//       "issues": [
+//           {
+//               "code": "too_small",
+//               "minimum": 6,
+//               "type": "string",
+//               "inclusive": true,
+//               "exact": false,
+//               "message": "6桁以上にしてください",
+//               "path": [
+//                   "id"
+//               ]
+//           }
+//       ],
+//       "name": "ZodError"
+//   }
+// }
+zodValidatorApp.post(
+  "/",
+  zValidator("json", schema, (result, c) => {
+    return c.json(result);
+  })
+);
+
+// routeはnest構造にできる
+app.route("/nest", nestApp).route("/zod", zodValidatorApp);
+
+export default app;
